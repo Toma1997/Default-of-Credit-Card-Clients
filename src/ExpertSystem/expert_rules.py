cc45498660b6f45f5dc038f4bfe9a0312b45f6d1
Toma1997 <toma.joksimovic@gmail.com>
2020-09-23 03:15:24 +0200

    Optimized classificatorm, added some rules

--- src/ExpertSystem/expert_rules.py
+++ src/ExpertSystem/expert_rules.py
@@ -8,40 +8,42 @@ def eq(x, y):
 
 # Decision context
 def define_ctxs(shell):
-    #sh.define_ctx(Ctx('client', ['sex', 'marriage','education']))
-    shell.define_ctx(Ctx('client', goals=['default_payment_next_month']))
+    shell.define_ctx(Ctx('client', ['LIMIT_BAL', 'SEX', 'EDUCATION', 'MARRIAGE', 'AGE', 'PAY_0', 'PAY_2', 'PAY_3', 'PAY_4', 'PAY_5', 'PAY_6', 
+                                'BILL_AMT1', 'BILL_AMT2', 'BILL_AMT3', 'BILL_AMT4', 'BILL_AMT5', 'BILL_AMT6', 'PAY_AMT1', 'PAY_AMT2', 'PAY_AMT3', 'PAY_AMT4', 'PAY_AMT5', 'PAY_AMT6']))
+    shell.define_ctx(Ctx('target', goals=['default_payment_next_month']))
+
+def yes_no_param(shell, param, ask_first = False):
+    shell.define_param(Param(param, 'client', enum=['no', 'yes']), ask_first = ask_first)
 
 # Parameters (attributes)
 def define_params(shell):
     
     # Client parameters
-    shell.define_param(Param('sex', 'client', enum=['male', 'female'], ask_first=True))
-    shell.define_param(Param('marriage', 'client', enum=['married', 'single', 'others'], ask_first=True))
-    shell.define_param(Param('education', 'client', enum=['graduate school', 'university', 'high school', 'others'], ask_first=True))
+
+    yes_no_param(shell, 'LIMIT_BAL', True)
+    yes_no_param(shell, 'SEX', True)
+    yes_no_param(shell, 'EDUCATION', True)
+    yes_no_param(shell, 'MARRIAGE', True)
+    yes_no_param(shell, 'AGE', True)
+    yes_no_param(shell, 'PAY_0')
+    yes_no_param(shell, 'PAY_2')
+    yes_no_param(shell, 'PAY_3')
+    yes_no_param(shell, 'PAY_4')
+    yes_no_param(shell, 'PAY_5')
+    yes_no_param(shell, 'PAY_6')
 
     # Class paramters (decisios) - default payment next month prediciton 
-    shell.define_param(Param('default_payment_next_month', 'client', enum=['yes', 'no']))
+    yes_no_param(shell, 'default_payment_next_month')
 
 # Production rules
 def define_rules(shell):
     # Rules
-    shell.define_rule(Rule(1,
-                        [('sex', 'client', eq, 'male')],
-                        [('default_payment_next_month', 'client', eq, 'yes')],
-			0.65))
-    shell.define_rule(Rule(2,
-                        [('marriage', 'client', eq, 'yes')],
-                        [('default_payment_next_month', 'client', eq, 'no')],
-			0.79))
-    shell.define_rule(Rule(3,
-                        [('education', 'client', eq, 'high school')],
-                        [('default_payment_next_month', 'client', eq, 'yes')],
-			0.75))
+    shell.define_rule(Rule(1, [('sex', 'client', eq, 'male')], [('default_payment_next_month', 'client', eq, 'yes')], 0.65))
+    shell.define_rule(Rule(2, [('marriage', 'client', eq, 'yes')], [('default_payment_next_month', 'client', eq, 'no')], 0.79))
+    shell.define_rule(Rule(3, [('education', 'client', eq, 'high school')], [('default_payment_next_month', 'client', eq, 'yes')], 0.75))
+    
     # Default rule
-    shell.define_rule(Rule(4,
-                        [],
-                        [('default_payment_next_month', 'client', eq, 'no')],
-			0.50))
+    shell.define_rule(Rule(4, [], [('default_payment_next_month', 'client', eq, 'no')], 0.0))
     
 # System execution
 from expert_shell import Param, Ctx, Rule, Shell
